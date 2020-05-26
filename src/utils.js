8a4c2dea6d14bbc92499022fba9245ef172913c1
Alexander Sokolov <sensetive5@yandex.ru>
2020-05-26 20:43:25 +0300

    Fix out tree

--- src/utils.js
+++ src/utils.js
@@ -1,55 +1,61 @@
-function getItemName (value) {
+function getItemName(value) {
   return value && value.hasOwnProperty('name') ? value.name : ''
 }
 
-function getChildItems (value) {
+function getChildItems(value) {
   return value && value.hasOwnProperty('items') ? value.items : []
 }
 
-function prepareJsonData (data) {
+function prepareJsonData(data) {
   return Array.isArray(data) ? data : [data]
 }
 
-function getSpacesSize (depth) {
+function getSpaces(depth, lastNotEndDepth, isEnd) {
   const SPACES_PATTERN = `    `
+  const NOT_END_PATTERN = `|   `
   let spacesString = ''
 
-  if (depth === 1) {
-    return spacesString
+  for (let i = 1; i < lastNotEndDepth; i++) {
+    spacesString += SPACES_PATTERN;
   }
 
-  for (let i = 1; i < depth; i++) {
-    spacesString += `${SPACES_PATTERN}`
+  for (let i = lastNotEndDepth; i < depth; ++i) {
+    spacesString += NOT_END_PATTERN;
   }
 
   return spacesString
 }
 
-function getCorrectPrefixSymbol (isEnd = false) {
+function getCorrectPrefixSymbol(isEnd = false) {
   const PARENT_PREFIX = '├──'
   const NEXT_LEVEL = '└──'
 
   return isEnd ? NEXT_LEVEL : PARENT_PREFIX
 }
 
-const outString = (item, depth, isEnd = false) => {
+const outString = (item, depth, lastNotEndDepth, isEnd = false) => {
   const prefixSymbol = getCorrectPrefixSymbol(isEnd)
 
   const name = getItemName(item)
-  const spaces = getSpacesSize(depth, isEnd)
+  const spaces = getSpaces(depth, lastNotEndDepth, isEnd)
   const finalString = `${spaces}${prefixSymbol} ${name}`
   console.log(finalString)
 }
 
-function shouldAddEndOfItemsSymbol (data, count) {
+function shouldAddEndOfItemsSymbol(data, count) {
   return (data.length - 1) === count
 }
 
-function parseJSONTree (data, depth = 0) {
-  depth++
+function parseJSONTree(data, depth = 0, lastNotEndDepth = 1) {
+  depth++;
   data.forEach((item, index) => {
-    outString(item, depth, shouldAddEndOfItemsSymbol(data, index))
-    parseJSONTree(getChildItems(item), depth)
+    const isEnd = shouldAddEndOfItemsSymbol(data, index);
+    outString(item, depth, lastNotEndDepth, isEnd)
+    if (isEnd) {
+      parseJSONTree(getChildItems(item), depth, lastNotEndDepth + 1)
+    } else {
+      parseJSONTree(getChildItems(item), depth, lastNotEndDepth)
+    }
   })
 }
 
