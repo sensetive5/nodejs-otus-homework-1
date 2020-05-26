6f9e8d2439efff179992167881373a56bc2d4222
Alexander Sokolov <sensetive5@yandex.ru>
2020-05-27 00:20:51 +0300

    Add Jest and minimist libs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const minimist = require("minimist");
 const { parseJSONTree, prepareJsonData } = require("./src/utils");
 
 const filePath = path.join(__dirname, "src/mocks/folders.json");
@@ -7,5 +8,15 @@ const filePath = path.join(__dirname, "src/mocks/folders.json");
 const rawData = fs.readFileSync(filePath);
 const parsedRawData = JSON.parse(rawData.toString());
 const preparedData = prepareJsonData(parsedRawData);
+
+const arguments = minimist(process.argv.slice(2), {
+  default: {
+    depth: 3,
+    path: null,
+  },
+});
+
+if (arguments.path === null)
+  throw new Error(`You should pass argument "path"!`);
 
 parseJSONTree(preparedData);
