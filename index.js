2b5571f77f3d8d7395108d2e0f5a22bff4d32c91
Alexander Sokolov <sensetive5@yandex.ru>
2020-05-27 00:10:47 +0300

    Fix path mock files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { parseJSONTree, prepareJsonData } = require("./src/utils");
 
-const filePath = path.join(__dirname, "/mocks/folders.json");
+const filePath = path.join(__dirname, "src/mocks/folders.json");
 
 const rawData = fs.readFileSync(filePath);
 const parsedRawData = JSON.parse(rawData.toString());
